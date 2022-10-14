47debe7b035352781287b69095de3c9430bd6b48
Amith <amitharun3@gmail.com>
2022-10-14 22:44:43 +0530

    added slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import Tabs from "./Components/Tabs/Tabs";
-import LInp from "./Components/LInp/LInp";
-import {Glass, Circle} from "./Components/Glass/Glass";
-import PyLink from "./Components/PyLink/PyLink";
-import Test from "./Components/Test";
-import DImg from "./Components/DImg/DImg";
+import Tabs from "./components/Tabs/Tabs";
+import LInp from "./components/LInp/LInp";
+import {Glass, Circle} from "./components/Glass/Glass";
+import PyLink from "./components/PyLink/PyLink";
+import Test from "./components/Test";
+import DImg from "./components/DImg/DImg";
+import Slider from "./components/Slider/Slider";
+
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => {
+        images[item.replace('./', '')] = r(item);
+    });
+    return images;
+}
+
+const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
 
 function CompRep({label, children}) {
     return (
@@ -65,6 +76,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
             </header>
+
             <CompRep label="Tabs">
                 <Tabs/>
             </CompRep>
@@ -79,6 +91,9 @@ function App() {
             </CompRep>
             <CompRep label="DImg">
                 <DImg/>
+            </CompRep>
+            <CompRep label="Slider">
+                <Slider imgs={Object.values(images)}/>
             </CompRep>
 
             {/*<CompRep label="Test">*/}
---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,39 @@
+import './Slider.css'
+import {useEffect, useState} from "react";
+
+export default function Slider({imgs, auto = 5000}) {
+    const [curr, setCurr] = useState(0);
+    let intervalId;
+
+    function inc(val, byAuto = false) {
+        setCurr(curr => {
+            curr += val;
+            curr %= imgs.length;
+            if (curr < 0) curr = imgs.length + curr;
+            return curr;
+        });
+        if (!byAuto) {
+            clearInterval(intervalId)
+            intervalId = setInterval(() => inc(1, true), auto);
+        }
+    }
+
+    useEffect(() => {
+        if (auto) {
+            intervalId = setInterval(() => inc(1, true), auto);
+            return () => clearInterval(intervalId);
+        }
+    }, []);
+    return (
+        <div className="Slider--cnt">
+            <div className="Slider">
+                {imgs.map(img =>
+                    <img key={img} alt={imgs.indexOf(img)} src={img} className="Img"
+                         active={img === imgs[curr] ? 1 : 0}/>
+                )}
+            </div>
+            <span className={"left"} onClick={() => inc(-1)}>{"<"}</span>
+            <span className={"right"} onClick={() => inc(1)}>{">"}</span>
+        </div>
+    );
+}
